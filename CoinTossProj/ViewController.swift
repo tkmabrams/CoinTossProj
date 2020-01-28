8ff004e4e9118ffafedc49ffbff3ca67693c6b90
Tash <taisheabrams@gmail.com>
2020-01-28 17:44:50 -0600

    Initial Commit

---
+++ CoinTossProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinTossProj
+//
+//  Created by Taishe Kenya on 1/28/20.
+//  Copyright © 2020 Taishe Kenya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
